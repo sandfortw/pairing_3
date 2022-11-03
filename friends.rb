71f4bc9a4828be55b800bfb1ef75704da6c4cfc3
Weston Sandfort <sandfortw@gmail.com>
2022-11-03 17:58:31 -0600

    Defines method under 60

--- friends.rb
+++ friends.rb
@@ -4,3 +4,13 @@ miles.each do |mile|
         puts mile
     end
 end
+
+def print_number_above_60
+    miles = [12, 3, 50, 60, 2, 1, 99, 100, 0]
+    miles.each do |mile|
+        if mile < 60
+            puts mile
+        end
+    end
+end
+print_number_above_60
