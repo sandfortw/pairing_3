c1f113922e29903b46cecb8f555b5a500ff8750a
Weston Sandfort <sandfortw@gmail.com>
2022-11-03 18:11:48 -0600

    Finalizes the code

--- friends.rb
+++ friends.rb
@@ -1,16 +1,20 @@
 miles = [12, 3, 50, 60, 2, 1, 99, 100, 0]
-miles.each do |mile|
-    if mile > 30
-        puts mile
-    end
-end
 
-def print_number_above_60
-    miles = [12, 3, 50, 60, 2, 1, 99, 100, 0]
-    miles.each do |mile|
-        if mile < 60
-            puts mile
+def print_number_above_30(number_array)
+number_array.each do |number|
+        if number > 30
+        puts number
         end
     end
 end
-print_number_above_60
+
+def print_number_under_60(number_array)
+    number_array.each do |number|
+        if number < 60
+            puts number
+        end
+    end
+end
+
+print_number_above_30(miles)
+print_number_under_60(miles)
